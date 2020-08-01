fcaf70f12c3b52d9662990f7aec5fcd0232ed414
Sasha Sushko <sushko@outlook.com>
2020-08-02 02:44:01 +0300

    Опишем презентер доски
    Презентер возьмет на себя (для начала) все те обязанности, что выполняла фунция renderBoard и renderTask.
    
    Начнем с переноса дочерних компонентов

---
+++ src/presenter/board.js
@@ -0,0 +1,18 @@
+import BoardView from "../view/board.js";
+import SortView from "../view/sort.js";
+import TaskListView from "../view/task-list.js";
+import NoTaskView from "../view/no-task.js";
+import LoadMoreButtonView from "../view/load-more-button.js";
+
+export default class BoardPresenter {
+  constructor(boardTasks) {
+    this._boardTasks = boardTasks;
+    this._boardComponent = new BoardView();
+    this._sortComponent = new SortView();
+    this._taskListComponent = new TaskListView();
+    this._noTaskComponent = new NoTaskView();
+    this._loadMoreButtonComponent = new LoadMoreButtonView();
+  }
+
+  render() {}
+}
